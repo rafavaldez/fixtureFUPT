acfe68d2766f8a3c01185444adb923fc15ab67ce
Rafael V <rafaelvaldezapaza@gmail.com>
2023-11-20 14:13:20 +0000

    gh

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.secret_key = 'clave_secreta'
@@ -7,8 +7,7 @@ app.secret_key = 'clave_secreta'
 def index():
     return render_template('index.html')
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
